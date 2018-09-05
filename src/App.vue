8fa0fd63760ecceee463074cf3eecbf89657b45d
ldrovira <luisdanielrovira8@gmail.com>
2018-09-05 18:08:20 -0400

    add hostjar

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,7 @@ export default {
     sidebarx
   },
   mounted () {
+    // this.getHot()
     window.addEventListener('scroll', this.scrollApp)
 
     this.$firebase.auth().onAuthStateChanged((user) => {
@@ -57,6 +58,17 @@ export default {
     })
   },
   methods: {
+    getHot () {
+      (function (h, o, t, j, a, r) {
+        h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) }
+        h._hjSettings = {hjid: 1004130, hjsv: 6}
+        a = o.getElementsByTagName('head')[0]
+        r = o.createElement('script')
+        r.async = 1
+        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
+        a.appendChild(r)
+      })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=')
+    },
     scrollApp (evt) {
       let posts = document.querySelectorAll('.post')
       let scrollTopx = document.documentElement.scrollTop
